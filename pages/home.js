57ad2cb32ef0d2fe923bb0adf2aa090f9857aaca
nforne <mnforne@gmail.com>
2023-02-09 13:31:02 -0500

    decrowd home of emojis and upgrade style to be more indicative of buisness purpose - better ux

--- pages/home.js
+++ pages/home.js
@@ -8,9 +8,9 @@ export const home = `
     <img class="gothicLine" src="./pics/gothicLine.png" alt="gothicLine" srcset="./pics/gothicLine.png">
     <div class='bg-light'>
       <img class="q2Logo" src="./pics/Q2bgfront.png" alt="q2Logo" srcset="./pics/Q2bgfront.png">
-      <div class="">
+      <div class="motto">
         <span class="welcome">[&nbsp; OUR OCCUPATION ? 🤗 &nbsp;]</span>
-        <span class="welcome">[&nbsp;🌼 | &nbsp; QUALITY & QUANTITY, &nbsp; 🌷 FOR YOU! 🍃 &nbsp; | 🚚&nbsp;]</span>
+        <span class="welcome">[&nbsp; 🛒 | &nbsp; QUALITY & QUANTITY, &nbsp; 🌼 FOR YOU! 🍃 &nbsp; | 🚚&nbsp;]</span>
       </div>
     </div>
     <img class="gothicLine" src="./pics/gothicLine.png" alt="gothicLine" srcset="./pics/gothicLine.png">
